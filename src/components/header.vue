b97f2b54023ce0c11049b20f80cdfffea681830f
罗亚男 <luoyanan@luoyanandeMacBook-Pro.local>
2018-05-17 22:17:38 +0800

    test

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,13 +1,28 @@
 <template>
-    <div>
+    <div id="header">
         <div class="page-cover"></div>
         <header>
-            <div class="icon"></div>
+            <div class="menu-icon"></div>
             <div class="title">全部</div>
-            <div class="tologin">登录</div>
+            <div class="tologin">&#xe60f;</div>
         </header>
     </div>
 </template>
 <style lang="less" scoped>
-
+    #header {
+        header{
+            display: flex;
+            .menu-icon {
+                width: 49px;
+                height: 44px;
+                position: absolute;
+                background: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACYAAAAgBAMAAACMSheAAAAABGdBT…VYdGRhdGU6bW9kaWZ5ADIwMTUtMTAtMTBUMTA6MDc6MTkrMDg6MDBBOXcbAAAAAElFTkSuQmCC) no-repeat 50%;
+                background-size: 19px 16px;
+                margin: 0;
+                z-index: 1;
+                top: 0;
+                left: 0;
+            }
+        }
+    }
 </style>
